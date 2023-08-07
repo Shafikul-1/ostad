3179569ba93da9ad731f858d2b00f3f584baae06
Shafikul <shafikul.18288@gmail.com>
2023-08-07 06:22:50 +0600

    ostad home Work

---
+++ product-img-item/src/data/imgdata.js
@@ -0,0 +1,150 @@
+const imgdata = [
+    {
+        content: {
+            heading: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz',
+            paragrape: `MORE SPEED AND POWER EFFICIENCY: The newest portable laptop comes equipped with a powerful 10th Generation`,
+            link: `https://www.daraz.com.bd/products/qbits-sigma-15-10th-gen-intel-core-i7-8gb-ddr4-3200mhz-ram-512gb-m2-nvme-ssd-10-i302965400-s1354840091.html?spm=a2a0e.searchlist.list.11.3e727463CYIewy&search=1`
+        },
+        mainImg: {
+            src: 'https://static-01.daraz.com.bd/p/e707c96efbea965a86f1d823c463f371.jpg_720x720.jpg_.webp',
+            alt: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz',
+            title: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz'
+        },
+        otherImages: [
+            {
+                src: 'https://static-01.daraz.com.bd/p/08d19dc77032d2fe61903fe3c6b5507c.jpg_720x720.jpg_.webp',
+                alt: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz',
+                title: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz'
+            },
+            {
+                src: 'https://static-01.daraz.com.bd/p/fd0e1bdccdd263c9065e99e9c4993f78.jpg_720x720.jpg_.webp',
+                alt: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz',
+                title: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz'
+            },
+            {
+                src: 'https://static-01.daraz.com.bd/p/f857a44c0e585b366012f445e8dd7f86.jpg_720x720.jpg_.webp',
+                alt: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz',
+                title: 'Qbits Sigma 15 Laptop 10th Gen Intel Core i7 Processor 8GB DDR4 3200MHz'
+            },
+            
+        ]
+    },
+    {
+        content: {
+            heading: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`,
+            paragrape: `MORE SPEED AND POWER EFFICIENCY: The newest portable laptop comes equipped with a powerful 10th Generation`,
+            link: `https://www.ryanscomputers.com/dell-vostro-14-3400-11th-gen-intel-core-i3-1115g4-4gb-ram-14-inch-hd-display-black-laptop`
+        },
+        mainImg: {
+            src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/dell-vostro-14-3400-11th-gen-intel-core-i3-1115g4-21675658356.webp`,
+            alt: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`,
+            title: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`
+        },
+        otherImages: [
+            {
+                src: `https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/vostro-notebooks/vostro-3400/pdp/vostro-14-3400-pdp-mod3-alt.jpg?fmt=jpg&wid=390&hei=240`,
+                alt: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`,
+                title: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/dell-vostro-14-3400-11th-gen-intel-core-i3-1115g4-41675658357.webp`,
+                alt: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`,
+                title: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/dell-vostro-14-3400-11th-gen-intel-core-i3-1115g4-31675658356.webp`,
+                alt: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`,
+                title: `Dell Vostro 14 3400 11th Gen Intel Core i3 1115G4 4GB RAM 14 Inch HD Display Black Laptop`
+            }
+        ]
+    },
+    {
+        content: {
+            heading: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`,
+            paragrape: `MORE SPEED AND POWER EFFICIENCY: The newest portable laptop comes equipped with a powerful 10th Generation`,
+            link: `https://www.ryanscomputers.com/acer-extensa-15-ex215-54-34se-intel-core-i3-1115g4-15.6-inch-fhd-charcoal-black-laptop`
+        },
+        mainImg:    {
+            src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/acer-extensa-15-ex215-54-34se-intel-core-i3-11648712667.webp`,
+            alt: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`,
+            title: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`
+        },
+        otherImages: [
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/acer-extensa-15-ex215-54-34se-intel-core-i3-21648712667.webp`,
+                alt: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`,
+                title: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/acer-extensa-15-ex215-54-34se-intel-core-i3-31648712668.webp`,
+                alt: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`,
+                title: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/acer-extensa-15-ex215-54-34se-intel-core-i3-41648712668.webp`,
+                alt: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`,
+                title: `Acer Extensa 15 EX215-54-34SE Intel Core i3 1115G4 15.6 Inch FHD Charcoal Black Laptop #NX.EGJSI.00B`
+            }
+        ]
+    },
+    {
+        content: {
+            heading: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`,
+            paragrape: `MORE SPEED AND POWER EFFICIENCY: The newest portable laptop comes equipped with a powerful 10th Generation`,
+            link: `https://www.ryanscomputers.com/lenovo-ideapad-1-15ada7-amd-ryzen-3-3250u-15.6-inch-fhd-display-cloud-grey-laptop`
+        },
+        mainImg:  {
+            src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-1-15ada7-amd-ryzen-3-3250u-156-11663994978.webp`,
+            alt: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`,
+            title: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`
+        },
+        otherImages: [
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-1-15ada7-amd-ryzen-3-3250u-156-21663994978.webp`,
+                alt: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`,
+                title: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-1-15ada7-amd-ryzen-3-3250u-156-31663994979.webp`,
+                alt: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`,
+                title: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-1-15ada7-amd-ryzen-3-3250u-156-41663994979.webp`,
+                alt: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`,
+                title: `Lenovo IdeaPad 1 15ADA7 AMD Ryzen 3 3250U 15.6 Inch FHD Display, Win 11, Cloud Grey Laptop`
+            }
+        ]
+    },
+    {
+        content: {
+            heading: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`,
+            paragrape: `MORE SPEED AND POWER EFFICIENCY: The newest portable laptop comes equipped with a powerful 10th Generation`,
+            link: `https://www.ryanscomputers.com/lenovo-ideapad-slim-3i-15igl-intel-cdc-n4020-15.6-inch-hd-display-platinum-grey-laptop-81wq00q2in-2y`
+        },
+        mainImg: {
+            src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-slim-3i-15igl-intel-cdc-n4020-156-11684154291.webp`,
+            alt: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`,
+            title: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`
+        },
+        otherImages: [
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-slim-3i-15igl-intel-cdc-n4020-156-21684154291.webp`,
+                alt: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`,
+                title: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-slim-3i-15igl-intel-cdc-n4020-156-31684154292.webp`,
+                alt: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`,
+                title: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`
+            },
+            {
+                src: `https://www.cloud.ryanscomputers.com/cdn/products/medium/lenovo-ideapad-slim-3i-15igl-intel-cdc-n4020-156-41684154292.webp`,
+                alt: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`,
+                title: `Lenovo IdeaPad Slim 3i 15IGL Intel CDC N4020 15.6 Inch HD Display Platinum Grey Laptop #81WQ00Q2IN-2Y`
+            }
+        ]
+    },
+    
+]
+export default imgdata
